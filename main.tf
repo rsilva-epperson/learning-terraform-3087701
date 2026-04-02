1d46eb4590ed33640f2cce375d3cb9990e540949
rsilva-epperson <rsilva@wfscorp.com>
2026-04-02 17:52:35 -0500

    Create VPC and security groups for blog instance
    Add VPC and security group resources for blog instance.

--- main.tf
+++ main.tf
@@ -14,11 +14,54 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+ vpc_security_group_ids = [aws_security_group.blog.id]
+
   tags = {
     Name = "HelloWorld"
   }
+}
+
+resource "aws_security_group" "blog" {
+  name        = "blog"
+  description = "Allow http and https in.  Allow everything out"
+
+vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog_https_in" {
+  type         = "ingress"
+  from_port    = 443
+  to _port     = 443
+  protocol     = "tcp"
+  cider_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_http_in" {
+  type         = "ingress"
+  from_port    = 80
+  to _port     = 80
+  protocol     = "tcp"
+  cider_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_http_out" {
+  type         = "engress"
+  from_port    = 0
+  to _port     = 0
+  protocol     = "-1"
+  cider_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
 }

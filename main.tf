19ff19847a183b15f3fffb32d7f599f525945492
rsilva-epperson <rsilva@wfscorp.com>
2026-04-07 15:35:42 -0500

    autoscaling dup removed

--- main.tf
+++ main.tf
@@ -91,15 +91,6 @@ resource "aws_lb_target_group_attachment" "blog" {
 }
 
 
-module "blog_autoscaling" {
-source  = "terraform-aws-modules/autoscaling/aws"
-version = "9.2.0"
-
-name = "blog"
-
-min
-
-}
 
 module "blog_autoscaling" {
 source  = "terraform-aws-modules/autoscaling/aws"
